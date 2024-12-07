cda0efe8679aa94ca22081810b024cdd3dbffe0a
sairamcrypto1 <90492312+sairamcrypto1@users.noreply.github.com>
2024-12-07 21:53:48 +0530

    Update

--- Program.cs
+++ Program.cs
@@ -105,7 +105,7 @@ namespace TrustWaveCarca
                 app.UseHsts();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
